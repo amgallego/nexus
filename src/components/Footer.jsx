ad192c9971889d0324383f34c9b5897750e65198
miguelmunoz-code <miguel.munoz@smartalentit.com>
2026-03-03 19:54:08 -0500

    Se crea componente Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,8 @@
+const Footer = () => (
+  <footer>
+    <p>📞 Medellín, Área Administrativa: (604) 000 000</p>
+    <p>✉️ Correo: comunicaciones@nexus.com.co</p>
+  </footer>
+);
+
+export default Footer;
